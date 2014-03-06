5b1b0545adb0924b05ab589ed206d450a01443db
Shichao An <shichao.an@nyu.edu>
2014-03-05 19:13:45 -0500

    Changed theme to responsive

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ disqus_shortname = None
 html_favicon = 'tinkerer.ico'
 
 # Pick another Tinkerer theme or use your own
-html_theme = "modern5"
+html_theme = "responsive"
 
 # Theme-specific options, see docs
 html_theme_options = { }
