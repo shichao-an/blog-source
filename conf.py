2a0ad1c39cb6a2835201156bbf5257d47fa3b4f9
Shichao An <shichao.an@nyu.edu>
2014-03-05 19:17:26 -0500

    Reverted theme to flat

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ disqus_shortname = None
 html_favicon = 'tinkerer.ico'
 
 # Pick another Tinkerer theme or use your own
-html_theme = "dark"
+html_theme = "flat"
 
 # Theme-specific options, see docs
 html_theme_options = { }
