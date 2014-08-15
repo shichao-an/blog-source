5f016fb20f15e2fd2d205671d52606a603359873
Shichao An <shichao.an@nyu.edu>
2014-08-15 01:56:34 -0400

    Added tag cloud

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ exclude_patterns = ["drafts/*", "_templates/*"]
 
 # Add templates to be rendered in sidebar here
 html_sidebars = {
-    "**": ["recent.html", "searchbox.html"]
+    "**": ["recent.html", "searchbox.html", "tags_cloud.html"]
 }
 
 # **************************************************************
