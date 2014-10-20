32b82cd0d5581840fc6e9cfd300e7370156124f7
Shichao An <shichao.an@nyu.edu>
2014-10-20 11:10:20 -0700

    Changed CNAME

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'Shichao An'
 copyright = '2014, ' + author
 
 # Change this to your blog root URL (required for RSS feed)
-website = 'http://blog.shichao-an.info/blog/html/'
+website = 'http://blog.shichao.io/blog/html/'
 
 # **************************************************************
 # More tweaks you can do
