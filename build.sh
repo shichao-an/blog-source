193287d6d1bcfc86519f9559dc2cd83ed73a84cf
Shichao An <shichao.an@nyu.edu>
2014-10-21 18:18:14 -0700

    Fixed

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
+# Directory for "shichao-an/blog" repo
 blog_html_dir="$(dirname $0)/../blog"
-echo $blog_html_dir
 tinker --build
+# Remove old files (dot files will not be affected)
 [ -n "$(ls $blog_html_dir)" ] && rm -rf $blog_html_dir/*
 cp -a blog/html/. $blog_html_dir
+# Copy CNAME
 echo 'blog.shichao.io' > $blog_html_dir/CNAME
