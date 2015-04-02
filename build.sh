e78d104a28913d484b6f0a9b047785819d0e7cb6
Shichao An <shichao.an@nyu.edu>
2015-04-01 19:00:30 -0700

    Added disqus.js sed command

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 blog_html_dir="$(dirname $0)/../blog"
 tinker --build
 gsed -i 's#http://fonts#https://fonts#g' blog/html/_static/flat.css
+gsed -i 's#http://#https://#g' blog/html/_static/disqus.js
 # Remove old files (dot files will not be affected)
 [ -n "$(ls $blog_html_dir)" ] && rm -rf $blog_html_dir/*
 cp -a blog/html/. $blog_html_dir
