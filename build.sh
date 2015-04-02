9ba70ca34c78735a7d7b86a5a3bd3eb60bf9aebe
Shichao An <shichao.an@nyu.edu>
2015-04-01 18:47:03 -0700

    Gsed https

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 # Directory for "shichao-an/blog" repo
 blog_html_dir="$(dirname $0)/../blog"
 tinker --build
+gsed -i 's#http://fonts#https://fonts#g' blog/html/_static/flat.css
 # Remove old files (dot files will not be affected)
 [ -n "$(ls $blog_html_dir)" ] && rm -rf $blog_html_dir/*
 cp -a blog/html/. $blog_html_dir
